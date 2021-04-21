0a01067550a0deeba956e8c99b17267d67d0eba9
JustTeRoR <pavlenok2000@gmail.com>
2021-04-21 08:30:40 +0300

    uploaded all classes for vin_range table. Filled for now with common requests

--- src/main/java/com/justterror/auto_kit/vinrange/boundary/VinRangeResource.java
+++ src/main/java/com/justterror/auto_kit/vinrange/boundary/VinRangeResource.java
@@ -1,4 +1,51 @@
 package com.justterror.auto_kit.vinrange.boundary;
 
+import com.justterror.auto_kit.vinrange.entity.VinRange;
+
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static com.justterror.auto_kit.security.Constants.ADMIN;
+import static com.justterror.auto_kit.security.Constants.USER;
+
+@ApplicationScoped
+@Path("/vin_range")
 public class VinRangeResource {
+
+    @Inject
+    Logger logger;
+
+    @Inject
+    private SecurityContext securityContext;
+
+    @Inject
+    private VinRangeService vinRangeService;
+
+    @GET
+    @Path("/all")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<VinRange> listAll()
+    {
+        logger.info("Get all VIN ranges");
+        return vinRangeService.getAll();
+    }
+
+    @GET
+    @Path("vin_range_id/{id}")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public VinRange getById(@PathParam("id") String id) {
+        logger.info("Get VIN ranges with id = " + id);
+        return vinRangeService.getById(id);
+    }
 }
--- src/main/java/com/justterror/auto_kit/vinrange/boundary/VinRangeService.java
+++ src/main/java/com/justterror/auto_kit/vinrange/boundary/VinRangeService.java
@@ -1,4 +1,33 @@
 package com.justterror.auto_kit.vinrange.boundary;
 
+import com.justterror.auto_kit.vinrange.entity.VinRange;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Stateless
 public class VinRangeService {
+
+    @Inject
+    Logger logger;
+
+    @PersistenceContext(name = "Auto-Kit")
+    private EntityManager entityManager;
+
+    public VinRange getById(String  id) {
+        String rawQuery = "select v from VinRange v where id = '" + id + "'";
+        TypedQuery<VinRange> query = entityManager.createQuery(rawQuery, VinRange.class);
+        return query.getSingleResult();
+    }
+
+    public List<VinRange> getAll() {
+
+        TypedQuery<VinRange> query = entityManager.createQuery("select v from VinRange v", VinRange.class);
+        return query.getResultList();
+    }
 }
