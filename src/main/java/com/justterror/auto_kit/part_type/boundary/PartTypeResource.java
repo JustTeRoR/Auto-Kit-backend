f1c8cbcd12f6d61ccace3d5f5c6e2b4d636f37ec
JustTeRoR <pavlenok2000@gmail.com>
2021-04-23 15:45:29 +0300

    implemented and debugged basic logic for part_type tables. Used new format of queries.

--- src/main/java/com/justterror/auto_kit/part_type/boundary/PartTypeResource.java
+++ src/main/java/com/justterror/auto_kit/part_type/boundary/PartTypeResource.java
@@ -1,4 +1,60 @@
 package com.justterror.auto_kit.part_type.boundary;
 
+import com.justterror.auto_kit.part_type.entity.PartType;
+
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.security.enterprise.SecurityContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static com.justterror.auto_kit.security.Constants.ADMIN;
+import static com.justterror.auto_kit.security.Constants.USER;
+
+@ApplicationScoped
+@Path("/part_type")
 public class PartTypeResource {
+
+    @Inject
+    Logger logger;
+
+    @Inject
+    private SecurityContext securityContext;
+
+    @Inject
+    private PartTypeService partTypeService;
+
+    @GET
+    @Path("/all")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PartType> listAll()
+    {
+        logger.info("Get all part types from part_type table");
+        return partTypeService.getAll();
+    }
+
+    @GET
+    @Path("/by_part_type_id")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public PartType getById(@QueryParam("id") long id) {
+        logger.info("Get part type with id = " + id);
+        return partTypeService.getById(id);
+    }
+
+    @GET
+    @Path("/by_name")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<PartType> getByPartIdField(@QueryParam("name") String name) {
+        logger.info("Get all part types from part_type table with name = " + name);
+        return partTypeService.getByName(name);
+    }
 }
--- src/main/java/com/justterror/auto_kit/part_type/entity/PartType.java
+++ src/main/java/com/justterror/auto_kit/part_type/entity/PartType.java
@@ -1,4 +1,73 @@
 package com.justterror.auto_kit.part_type.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "part_type", schema = "public")
 public class PartType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="name")
+    @NotNull
+    private String name;
+
+    @Column(name="description")
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PartType partType = (PartType) o;
+
+        if (id != partType.id) return false;
+        if (!name.equals(partType.name)) return false;
+        return description != null ? description.equals(partType.description) : partType.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "PartType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
