71c96e66ed4fb226138b6ad365b58b1a179b1bad
JustTeRoR <pavlenok2000@gmail.com>
2021-04-23 15:44:44 +0300

    implemented and debugged basic logic for part_type tables. Used new format of queries.

---
+++ src/main/java/com/justterror/auto_kit/part_type/boundary/PartTypeService.java
@@ -0,0 +1,4 @@
+package com.justterror.auto_kit.part_type.boundary;
+
+public class PartTypeService {
+}
---
+++ src/main/java/com/justterror/auto_kit/part_type/entity/PartType.java
@@ -0,0 +1,4 @@
+package com.justterror.auto_kit.part_type.entity;
+
+public class PartType {
+}
