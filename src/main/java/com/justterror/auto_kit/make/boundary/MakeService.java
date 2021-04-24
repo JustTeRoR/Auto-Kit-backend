ee637d11a074575004984c5e96d6fb5c7ade4a1a
JustTeRoR <pavlenok2000@gmail.com>
2021-04-24 21:56:00 +0300

    Implemented basic logic for measure and make  table. In future - add business calls and debug.

---
+++ src/main/java/com/justterror/auto_kit/make/boundary/MakeService.java
@@ -0,0 +1,45 @@
+package com.justterror.auto_kit.make.boundary;
+
+import com.justterror.auto_kit.make.entity.Make;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.logging.Logger;
+
+//TODO:: debug this
+
+@Stateless
+public class MakeService {
+    @Inject
+    Logger logger;
+
+    @PersistenceContext(name = "Auto-Kit")
+    private EntityManager entityManager;
+
+    public Make getById(long  id) {
+        String rawQuery = String.format("FROM Make WHERE id = %d", id);
+        TypedQuery<Make> query = entityManager.createQuery(rawQuery, Make.class);
+        return query.getSingleResult();
+    }
+
+    public List<Make> getAll() {
+        TypedQuery<Make> query = entityManager.createQuery("select p from Make p", Make.class);
+        return query.getResultList();
+    }
+
+    public List<Make> getAllByName(String name) {
+        String rawQuery = String.format("FROM Make WHERE name = '%s'", name);
+        TypedQuery<Make> query = entityManager.createQuery(rawQuery, Make.class);
+        return query.getResultList();
+    }
+
+    public List<Make> getAllByVpicId(long vpicId) {
+        String rawQuery = String.format("FROM Make WHERE vpic_id = %d", vpicId);
+        TypedQuery<Make> query = entityManager.createQuery(rawQuery, Make.class);
+        return query.getResultList();
+    }
+}
---
+++ src/main/java/com/justterror/auto_kit/make/entity/Make.java
@@ -0,0 +1,73 @@
+package com.justterror.auto_kit.make.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "make", schema = "public")
+public class Make {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="vpic_id")
+    private long vpicId;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getVpicId() {
+        return vpicId;
+    }
+
+    public void setVpicId(long vpicId) {
+        this.vpicId = vpicId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Make make = (Make) o;
+
+        if (id != make.id) return false;
+        if (vpicId != make.vpicId) return false;
+        return name.equals(make.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (int) (vpicId ^ (vpicId >>> 32));
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Make{" +
+                "id=" + id +
+                ", vpicId=" + vpicId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
