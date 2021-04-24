33e53a40a9cf419cd51ae636b28773b84df2a7c1
JustTeRoR <pavlenok2000@gmail.com>
2021-04-24 21:17:32 +0300

    Implemented basic logic for model table. In future - add business calls and debug.

---
+++ src/main/java/com/justterror/auto_kit/model/boundary/ModelResource.java
@@ -0,0 +1,108 @@
+package com.justterror.auto_kit.model.boundary;
+
+import com.justterror.auto_kit.model.entity.Model;
+
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.security.enterprise.SecurityContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.justterror.auto_kit.security.Constants.ADMIN;
+import static com.justterror.auto_kit.security.Constants.USER;
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+
+//TODO:: to debug this
+
+@ApplicationScoped
+@Path("/model")
+public class ModelResource {
+
+    @Inject
+    Logger logger;
+
+    @Inject
+    private SecurityContext securityContext;
+
+    @Inject
+    private ModelService modelService;
+
+    @GET
+    @Path("/all")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Model> listAll()
+    {
+        logger.info("Get all models ");
+        return modelService.getAll();
+    }
+
+    @GET
+    @Path("/by_model_id")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Model getById(@QueryParam("id") long id) {
+        logger.info("Get Model with id = " + id);
+        return modelService.getById(id);
+    }
+
+    @GET
+    @Path("/by_name")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Model> getByName(@QueryParam("name") String name) {
+        logger.info("Get Model with name = " + name);
+        return modelService.getAllByName(name);
+    }
+
+    @GET
+    @Path("/by_make_id")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Model> getByMakeId(@QueryParam("make_id") long makeId) {
+        logger.info("Get Model with make_id = " + makeId);
+        return modelService.getAllByMakeID(makeId);
+    }
+
+    @POST
+    @Path("/insert")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createNewOrder(@QueryParam("make_id") long makeId, @QueryParam("vpic_id") long vpicId,
+                                   @QueryParam("name") String name, @QueryParam("trim_name") String trim_name ) throws SQLException {
+        logger.log(Level.INFO, String.format("Inserting new model with parameters: make_id = %d, name = %s", makeId, name));
+        try {
+            modelService.insertModelTODB(makeId,vpicId,name,trim_name);
+            return Response.ok().build();
+        } catch (SQLException exeption) {
+            logger.log(Level.WARNING, String.format("ERROR on inserting model with parameters: make_id = %d, name = %s", makeId, name));
+            return Response.status(INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DELETE
+    @Path("/delete_by_id")
+    @RolesAllowed({USER, ADMIN})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteOrderPartByID(@QueryParam("id") long id) throws SQLException{
+        logger.log(Level.INFO, String.format("Deleting model with id = %d", id));
+        try {
+            modelService.deleteModelByID(id);
+            return Response.ok().build();
+        } catch (SQLException exception) {
+            logger.log(Level.WARNING, String.format("ERROR on deleting model with id = %d", id));
+            return Response.status(INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+}
---
+++ src/main/java/com/justterror/auto_kit/model/boundary/ModelService.java
@@ -0,0 +1,61 @@
+package com.justterror.auto_kit.model.boundary;
+
+import com.justterror.auto_kit.model.entity.Model;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Stateless
+public class ModelService {
+
+    @Inject
+    Logger logger;
+
+    @PersistenceContext(name = "Auto-Kit")
+    private EntityManager entityManager;
+
+    public Model getById(long  id) {
+        String rawQuery = String.format("FROM Model WHERE id = %d", id);
+        TypedQuery<Model> query = entityManager.createQuery(rawQuery, Model.class);
+        return query.getSingleResult();
+    }
+
+    public List<Model> getAll() {
+        TypedQuery<Model> query = entityManager.createQuery("select p from Model p", Model.class);
+        return query.getResultList();
+    }
+
+    public List<Model> getAllByName(String name) {
+        String rawQuery = String.format("FROM Model WHERE name = '%s'", name);
+        TypedQuery<Model> query = entityManager.createQuery(rawQuery, Model.class);
+        return query.getResultList();
+    }
+
+    public List<Model> getAllByMakeID(long makeId) {
+        String rawQuery = String.format("FROM Model WHERE make_id = %d", makeId);
+        TypedQuery<Model> query = entityManager.createQuery(rawQuery, Model.class);
+        return query.getResultList();
+    }
+
+    public void insertModelTODB(long make_id, long vpicId, String name, String trimName) throws SQLException {
+        String queryString = String.format("INSERT INTO \"model\" (make_id, vpic_id, name, trim_name) VALUES (%d, %d, %s, %s)",
+                make_id, vpicId, name, trimName);
+        Query query= entityManager.createNativeQuery(queryString);
+        query.executeUpdate();
+    }
+
+    public void deleteModelByID(long id) throws SQLException {
+        String queryString = String.format("DELETE FROM \"model\" WHERE id IN ('%d')", id);
+        Query query= entityManager.createNativeQuery(queryString);
+        query.executeUpdate();
+    }
+}
